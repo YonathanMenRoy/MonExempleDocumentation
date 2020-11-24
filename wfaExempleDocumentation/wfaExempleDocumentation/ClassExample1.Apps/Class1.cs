b2faa772fcab6d5aee33359542a168ecdfa95291
Yonathan Mendez-Roy <1333286@cegepba.qc.ca>
2020-11-24 09:49:07 -0500

    intial commit

---
+++ wfaExempleDocumentation/wfaExempleDocumentation/ClassExample1.Apps/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wfaExempleDocumentation;
+
+namespace ClassExample1.Apps
+{
+    public class Class1 : IExemple
+    {
+
+    }
+}
---
+++ wfaExempleDocumentation/wfaExempleDocumentation/IExemple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfaExempleDocumentation
+{
+    /// <summary>
+    /// This interface is an exemple for documentation.
+    /// </summary>
+    /// 
+    public interface IExemple
+    {
+        /// <summary>
+        /// This function is here only to show an example for documentation.
+        /// </summary>
+        /// <param name="param1"> First parameter (to add)</param>
+        /// <param name="param2">Second parameter (to add)</param>
+        /// <returns> An int , this function could add two integers. </returns>
+        int ExampleFunction(int param1, int param2);
+    }
+}
